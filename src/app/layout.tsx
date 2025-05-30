347106c18b580c887bb90eecf2ad3a92053d8b0e
tdnogueira <nogueira.tcs@gmail.com>
2025-05-29 22:23:29 -0300

    [IT - LandingPage] Erro deploy vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,4 @@
-import { Poppins } from "next/font/google";
 import "./globals.css";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "600", "700"],
-  variable: "--font-poppins",
-});
 
 export const metadata = {
   title: "IT",
